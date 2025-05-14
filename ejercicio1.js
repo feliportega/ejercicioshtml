f51797d2dd26b4e622716c863961178175d0a74b
vargas023 <alejandravargasb1@gmail.com>
2025-05-13 21:29:22 -0500

    formularios

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+saludo="hola, este es mi ejercicio 1";
+console.log(saludo);
+let num=8;
+console.log("el numero es: "+num);
+//trabajando caracteres con una sola comilla
+let caracter = 'a';
+console.log("el caracter es: "+caracter);
+/*
+trabajando con tipos de datos de javascript
+*/
+let decimal = 3.1416;
+console.log("el decimal es: "+decimal);
+console.log(`el decimal es ${decimal} y el numero es ${num}`);
