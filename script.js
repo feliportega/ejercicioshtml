882367a0d872c72476cfb51025c1bc4bf9ff72bc
Juls2404 <juliethximenarendonsanchez@gmail.com>
2025-05-23 22:53:42 -0500

    Estructura semantica

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('a.scroll').forEach(enlace => {
+  enlace.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
